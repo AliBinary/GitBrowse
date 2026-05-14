27c26939927fac5440931cc7e699297def71b753
Ali Ghanbari <Ali.Ghanbari.Dev@gmail.com>
2026-05-14 07:41:17 +0330

    Delete downloads directory

--- downloads/Mindset-For-IELTS-Foundation-Unit-06-to-10/reassemble.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Reassemble Mindset-For-IELTS-Foundation-Unit-06-to-10.rar
-# Run this script in the same folder as all the .part files
-
-cat Mindset-For-IELTS-Foundation-Unit-06-to-10.rar.*.part > Mindset-For-IELTS-Foundation-Unit-06-to-10.rar
-echo "Reassembled: Mindset-For-IELTS-Foundation-Unit-06-to-10.rar"
